8ccdd3782b02cb52e7942a37b2b3562b7e5973f2
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2019-10-10 22:59:51 +0300

    Tests were added

---
+++ tests/index.test.js
@@ -0,0 +1,54 @@
+const asyncContext = require("../src/index");
+const defaultContext = asyncContext.defaultContext;
+
+test("Must pass context to child asynchronous operation", async () => {
+  // Given
+  const key = "counter";
+  const initialValue = 3;
+
+  // When
+  asyncContext.create().set(key, initialValue);
+  const actualValue = await new Promise(resolve => resolve(defaultContext.get(key) + 2));
+
+  // Then
+  const expectedValue = 5;
+  expect(actualValue).toBe(expectedValue);
+});
+
+test("Must not corrupt parent context", async () => {
+  // Given
+  const key = "counter";
+  const initialValue = 3;
+
+  // When
+  asyncContext.create().set(key, initialValue);
+  const actualValue = await new Promise(async resolve => {
+    await new Promise(childResolve => {
+      defaultContext.set(key, defaultContext.get(key) + 2);
+      childResolve();
+    });
+    resolve(defaultContext.get(key));
+  });
+
+  // Then
+  expect(actualValue).toBe(initialValue);
+});
+
+test("Must pass named context to child asynchronous operation", async () => {
+  // Given
+  const firstKey = "firstCounter";
+  const firstValue = 3;
+
+  const contextName = "NamedContext";
+  const secondKey = "secondCounter";
+  const secondValue = 1;
+
+  // When
+  asyncContext.create().set(firstKey, firstValue);
+  asyncContext.create(contextName).set(secondKey, secondValue);
+  const actualValue = await new Promise(resolve => resolve(asyncContext.obtain(contextName).get(secondKey) + 2));
+
+  // Then
+  const expectedValue = 3;
+  expect(actualValue).toBe(expectedValue);
+});
